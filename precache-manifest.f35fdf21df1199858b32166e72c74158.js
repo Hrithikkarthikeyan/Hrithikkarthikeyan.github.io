1fe0709975ef1cb1f99630f55e4361c127ab6d7c
Hrithikkarthikeyan <kk8008988901@gmail.com>
2024-02-29 23:24:12 +0530

    Updates

---
+++ precache-manifest.f35fdf21df1199858b32166e72c74158.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20dec1947e6a063c30a873a5f84a5397",
+    "url": "/index.html"
+  },
+  {
+    "revision": "030c6ab733a0243564bd",
+    "url": "/static/css/2.35424416.chunk.css"
+  },
+  {
+    "revision": "b382240f501c49d4d0b5",
+    "url": "/static/css/main.7ade0128.chunk.css"
+  },
+  {
+    "revision": "030c6ab733a0243564bd",
+    "url": "/static/js/2.4cdedf48.chunk.js"
+  },
+  {
+    "revision": "363eb864de657bac321764863e583766",
+    "url": "/static/js/2.4cdedf48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f239b83e71035d6067e5",
+    "url": "/static/js/3.4f2329f5.chunk.js"
+  },
+  {
+    "revision": "b382240f501c49d4d0b5",
+    "url": "/static/js/main.e70b8dd5.chunk.js"
+  },
+  {
+    "revision": "9c2233ea64485ea53cf4",
+    "url": "/static/js/runtime-main.e2c4b05b.js"
+  },
+  {
+    "revision": "5b4cd7744a7e2bc3e4eeceab709823ad",
+    "url": "/static/media/Hrithik.5b4cd774.jpg"
+  }
+]);
